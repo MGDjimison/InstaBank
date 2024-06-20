d0f5891093be9991ba1c768ddbec0a0847f8f9d2
djimi <djimi9530@gmail.com>
2024-06-20 21:07:29 +0200

    contrainte dans formulaire d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,17 +4,16 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\LessThanOrEqual;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -23,9 +22,23 @@ class RegistrationFormType extends AbstractType
         $builder
             ->add('nom',TextType::class, [
                 'attr' => ['class' => 'm-3'],
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/\d/',
+                        'match' => false,
+                        'message' => 'Votre nom ne peut pas contenir de nombres'
+                    ])
+                ]
             ])
             ->add('prenom',TextType::class, [
                 'attr' => ['class' => 'm-3'],
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/\d/',
+                        'match' => false,
+                        'message' => 'Votre prénom ne peut pas contenir de nombres'
+                    ])
+                ]
             ])
             ->add('date_naissance', DateType::class, [
                 'label' => 'Date de naissance',
